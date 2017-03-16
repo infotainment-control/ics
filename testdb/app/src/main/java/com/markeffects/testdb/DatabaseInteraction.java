3dfc4771a7200e59e7a01564f5e04be54954a440
mark.bank <mark.bank@ibm.com>
2017-03-16 17:56:21 -0400

    w.i.p. a hello world for the sqlite backend

--- testdb/app/src/main/java/com/markeffects/testdb/DatabaseInteraction.java
+++ testdb/app/src/main/java/com/markeffects/testdb/DatabaseInteraction.java
@@ -1,48 +1,99 @@
 package com.markeffects.testdb;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.Date;
+
 public class DatabaseInteraction extends AppCompatActivity {
+
+    private static final String TAG = "[MAIN_ACTIVITY]";
 
     String myStr = "hihihihihihi.";
     String other = "bye bye bye!!";
 
     boolean oneOrTheOther = false;
 
+    OpenHelper dbHelper;// = new FeedReaderDbHelper(getContext());
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database_interaction);
+
+
+        dbHelper = new OpenHelper(getApplicationContext());
+        SQLiteDatabase sqlDB = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                OpenHelper.MESSAGE_COL_NAME,
+                OpenHelper.TIMESTAMP_COL_NAME
+        };
+
+        Cursor resultCursor = sqlDB.query(OpenHelper.MY_TABLE_NAME, projection, null, null, null, null, null);
+
+        int messageColIndex   = resultCursor.getColumnIndex(OpenHelper.MESSAGE_COL_NAME  );
+        int timestampColIndex = resultCursor.getColumnIndex(OpenHelper.TIMESTAMP_COL_NAME);
+
+        while(resultCursor.moveToNext()) {
+            String message = resultCursor.getString(messageColIndex);
+            Date timestamp = new Date(resultCursor.getInt(timestampColIndex) * 1000);
+
+            Log.d(TAG, "message: " + message);
+            Log.d(TAG, "created: " + timestamp.toString());
+        }
+
+//        List itemIds = new ArrayList<>();
+//        while(cursor.moveToNext()) {
+//            long itemId = cursor.getLong(
+//                    cursor.getColumnIndexOrThrow(FeedEntry._ID));
+//            itemIds.add(itemId);
+//        }
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //final TextView text = (TextView) findViewById(R.id.my_text_field);
+        final TextView text = (TextView) findViewById(R.id.message);
 
-        final TextView text = (TextView) findViewById(R.id.my_text_field);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
 
-                String next = (oneOrTheOther)
-                        ? myStr
-                        : other;
+//                String next = (oneOrTheOther)
+//                        ? myStr
+//                        : other;
+//
+//                text.setText(next);
 
-                text.setText(next);
+//                oneOrTheOther = ! oneOrTheOther;
 
-                oneOrTheOther = ! oneOrTheOther;
+                String inputMessage = text.getText().toString();
+                if (! inputMessage.equals(getResources().getString(R.string.default_message))) {
+                    Log.d(TAG, "trying to submit default entry -> no op");
+                }
 
             }
         });
+
+        resultCursor.close();
     }
 
     @Override
--- testdb/app/src/main/java/com/markeffects/testdb/OpenHelper.java
+++ testdb/app/src/main/java/com/markeffects/testdb/OpenHelper.java
@@ -1,8 +1,14 @@
 package com.markeffects.testdb;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -10,7 +16,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class OpenHelper extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
-    private static final String MY_DATABASE_NAME = "HeyThere";
+
+    static final String MY_DATABASE_NAME   = "HeyThere";
+    static final String MY_TABLE_NAME      = "Statuses";
+
+    static final String MESSAGE_COL_NAME   = "message";
+    static final String TIMESTAMP_COL_NAME = "timestamp";
+
+    private static final String CREATE_DATABASE_COMMAND =
+            "CREATE TABLE " + MY_TABLE_NAME +
+            " ("
+                 + MESSAGE_COL_NAME   + " TEXT, "
+                 + TIMESTAMP_COL_NAME + " DATETIME" +
+            ");";
+
+    private static final String TAG = "[OPEN_HELPER]";
 
     OpenHelper(Context context) {
         super(context, MY_DATABASE_NAME, null, VERSION);
@@ -18,12 +38,30 @@ public class OpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_DATABASE_COMMAND);
+        populateDatabase(db);
+    }
 
+    private void populateDatabase(SQLiteDatabase db) {
+        // for optimizing when addressing the large insert of the device data, see blog post at
+        // http://www.outofwhatbox.com/blog/2010/12/android-using-databaseutils-inserthelper-for-faster-insertions-into-sqlite-database/
+
+        ContentValues contentValues = new ContentValues();
+
+        List<String> messages = new ArrayList<>(5);
+        messages.add("msg 1"); messages.add("msg 2"); messages.add("HI"); messages.add("msg 4"); messages.add("msg 5");
+
+        for (String message : messages) {
+            contentValues.put(MESSAGE_COL_NAME, message);
+            contentValues.put(TIMESTAMP_COL_NAME, new Date().getTime() / 1000);
+        }
+
+        db.insert(MY_TABLE_NAME, null, contentValues);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Log.d(TAG, "unsupported operation. we're not versioning our internal database!");
     }
 }
 
