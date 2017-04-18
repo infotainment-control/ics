571c332880baa72eacae89385a4e0f05ea463833
jaason435 <jaason435@gmail.com>
2017-04-18 10:29:59 -0400

    Started new settings page

--- Infortainment_Control/app/src/main/java/ics/infortainment_control/settings_fragment.java
+++ Infortainment_Control/app/src/main/java/ics/infortainment_control/settings_fragment.java
@@ -18,19 +18,9 @@ import java.text.DecimalFormat;
 
 public class settings_fragment extends Fragment {
 
-
-    Button add_tv_btn;
-
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup containter, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.settings_layout,containter,false);
-        add_tv_btn = (Button) v.findViewById(R.id.add_television_btn);
-        add_tv_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                add_tv_fragment f = new add_tv_fragment();
-                getFragmentManager().beginTransaction().replace(R.id.frame,f).commit();
-            }
-        });
+
 
         return v;
     }
