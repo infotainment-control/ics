b019fb47d96a0630f0e6ad1f8771c70da1772810
Jaedyn1365 <spsnyder@kent.edu>
2017-04-19 22:49:26 -0400

    Add files via upload

---
+++ Infortainment_Control/app/src/main/java/ics/infortainment_control/app/App.java
@@ -0,0 +1,29 @@
+package ics.infortainment_control.app;
+
+
+import android.app.Application;
+import android.content.Context;
+
+import ics.infortainment_control.devices.Data.DBHelper;
+import ics.infortainment_control.devices.Data.DatabaseManager;
+
+
+public class  App extends Application {
+    private static Context context;
+    private static DBHelper dbHelper;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        context = this.getApplicationContext();
+        dbHelper = new DBHelper();
+        DatabaseManager.initializeInstance(dbHelper);
+
+    }
+
+    public static Context getContext(){
+        return context;
+    }
+
+}
