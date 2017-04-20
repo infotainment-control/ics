980f551feab2db1236be81d2123de9e98677a56b
mark.bank <mark.bank@ibm.com>
2017-04-20 13:28:11 -0400

    refactoring command enumeration

--- Infortainment_Control/app/src/main/java/ics/infortainment_control/commands/Command.java
+++ Infortainment_Control/app/src/main/java/ics/infortainment_control/commands/Command.java
@@ -1,35 +1,52 @@
 package ics.infortainment_control.commands;
 
 /**
- *  Enumeration for dynamic instantiation of TV device command implementations.
+ *  Enumeration for dynamic instantiation of device command implementations.
  */
 public enum Command {
 
-    POWER       ("power",        "turns the device on and off"                      ),
-    VOLUME_UP   ("volume up",    "raises the device's level of volume"              ),
-    VOLUME_DOWN ("volume down",  "lowers the device's level of volume"              ),
-    CHANNEL_UP  ("channel up",   "increments the device channel tuned in to"        ),
-    CHANNEL_DOWN("channel down", "decrements the device channel tuned in to"        ),
-    SOURCE      ("source",       "changes the input source of the device"           ),
-    MENU        ("menu",         "opens the device's settings menu"                 ),
-    UP          ("up",           "selects an option above the current selection"    ),
-    DOWN        ("down",         "selects an option below the current selection"    ),
-    LEFT        ("left",         "selects an option left of the current selection"  ),
-    RIGHT       ("right",        "selects an option right of the current selection" ),
-    ENTER       ("enter",        "affirms a selected option"                        ),
-    EXIT        ("exit",         "cancels a selection or returns to a previous menu"),
-    MUTE        ("mute",         "silences and unsilences the device"               ),
-    INFO        ("info",         "displays additional information"                  ),
-    ZERO        ("0",            "the digit 0"                                      ),
-    ONE         ("1",            "the digit 1"                                      ),
-    TWO         ("2",            "the digit 2"                                      ),
-    THREE       ("3",            "the digit 3"                                      ),
-    FOUR        ("4",            "the digit 4"                                      ),
-    FIVE        ("5",            "the digit 5"                                      ),
-    SIX         ("6",            "the digit 6"                                      ),
-    SEVEN       ("7",            "the digit 7"                                      ),
-    EIGHT       ("8",            "the digit 8"                                      ),
-    NINE        ("9",            "the digit 9"                                      );
+    CHANNEL_DOWN ("channel down", "decrements the device channel tuned in to"         ),
+    CHANNEL_UP   ("channel up",   "increments the device channel tuned in to"         ),
+    DISPLAY      ("",             ""                                                  ),
+    DOWN         ("down",         "selects an option below the current selection"     ),
+    EIGHT        ("8",            "the digit 8"                                       ),
+    EJECT        ("",             ""                                                  ),
+    ENTER        ("enter",        "affirms a selected option"                         ),
+    EXIT         ("exit",         "cancels a selection or returns to a previous menu" ),
+    FASTFORWARD  ("",             ""                                                  ),
+    FIVE         ("5",            "the digit 5"                                       ),
+    FORWARD_STEP ("",             ""                                                  ),
+    FOUR         ("4",            "the digit 4"                                       ),
+    GUIDE        ("",             ""                                                  ),
+    INFO         ("info",         "displays additional information"                   ),
+    LEFT         ("left",         "selects an option left of the current selection"   ),
+    MENU         ("menu",         "opens the device's settings menu"                  ),
+    MODE         ("",             ""                                                  ),
+    MUTE         ("mute",         "silences and unsilences the device"                ),
+    NINE         ("9",            "the digit 9"                                       ),
+    ONE          ("1",            "the digit 1"                                       ),
+    PAUSE        ("",             ""                                                  ),
+    PLAY         ("",             ""                                                  ),
+    POWER        ("power",        "turns the device on and off"                       ),
+    RECALL       ("",             ""                                                  ),
+    RECORD       ("",             ""                                                  ),
+    REVERSE_STEP ("",             ""                                                  ),
+    REWIND       ("",             ""                                                  ),
+    RIGHT        ("right",        "selects an option right of the current selection"  ),
+    SEVEN        ("7",            "the digit 7"                                       ),
+    SIX          ("6",            "the digit 6"                                       ),
+    SLOW         ("",             ""                                                  ),
+    SOURCE       ("source",       "changes the input source of the device"            ),
+    STOP         ("",             ""                                                  ),
+    SUBTITLE     ("",             ""                                                  ),
+    THREE        ("3",            "the digit 3"                                       ),
+    TITLE        ("",             ""                                                  ),
+    TWO          ("2",            "the digit 2"                                       ),
+    UP           ("up",           "selects an option above the current selection"     ),
+    VOLUME_DOWN  ("volume down",  "lowers the device's level of volume"               ),
+    VOLUME_UP    ("volume up",    "raises the device's level of volume"               ),
+    ZERO         ("0",            "the digit 0"                                       );
+
 
     private final String simpleName;
     private final String description;
